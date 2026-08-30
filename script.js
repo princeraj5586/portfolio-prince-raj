dd3b1a06dc53aa6435f129d926c406dd64931f02
prince raj <princeraj09596@gmail.com>
2026-08-31 00:26:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const nav = document.querySelector('.navbar');
+const menu = document.querySelector('.menu');
+menu.addEventListener('click', () => nav.classList.toggle('open'));
+document.querySelectorAll('nav a').forEach(a => a.addEventListener('click', () => nav.classList.remove('open')));
